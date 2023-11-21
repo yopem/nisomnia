c1749373d75e4d3590ac783f7ee4e4d04f879fc9
karyanayandi <halo@yandi.me>
2023-11-21 22:13:59 +0700

    fix(apps/web): sitemap url in robots.txt

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "*",
     },
-    sitemap: env.NEXT_PUBLIC_SITE_URL,
+    sitemap: `${env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   }
 }
