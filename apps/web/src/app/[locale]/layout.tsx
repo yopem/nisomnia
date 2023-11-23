bd57f3668a885eb729da1f84cfc23e2bb9e5ca0e
karyanayandi <halo@yandi.me>
2023-11-23 17:32:51 +0700

    fix(apps/web): adsense error

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -96,9 +96,6 @@ export default function RootLayout({
             <Script
               id="adsense"
               async
-              onError={(e) => {
-                console.error("Script failed to load", e)
-              }}
               src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
               crossOrigin="anonymous"
               strategy="beforeInteractive"
