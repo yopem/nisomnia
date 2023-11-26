9fe26458e876eb4820cb4578795de14331c7e446
karyanayandi <halo@yandi.me>
2023-11-26 12:47:32 +0700

    feat(apps/web): change adsense strategi to beforeInteractive

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -96,7 +96,7 @@ export default function RootLayout({
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
             crossOrigin="anonymous"
-            strategy="lazyOnload"
+            strategy="beforeInteractive"
           />
         )}
       </body>
