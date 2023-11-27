23be837ee48bed5b2219f906489202456c437c6b
karyanayandi <halo@yandi.me>
2023-11-27 12:19:23 +0700

    fix(apps/web): change ad content as children on adsense  component

--- apps/web/src/components/Ad/client/Adsense.tsx
+++ apps/web/src/components/Ad/client/Adsense.tsx
@@ -1,48 +1,39 @@
 "use client"
 
 import * as React from "react"
+import { usePathname, useSearchParams } from "next/navigation"
 
 import { Skeleton } from "@nisomnia/ui"
 
-import env from "@/env"
-
 interface AdsenseProps extends React.HTMLAttributes<HTMLDivElement> {
-  content: string
+  children: React.ReactNode
 }
 
 export const Adsense: React.FunctionComponent<AdsenseProps> = (props) => {
-  const { content } = props
+  const { children } = props
 
   const [loading, setLoading] = React.useState<boolean>(true)
+
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
 
   React.useEffect(() => {
     setLoading(true)
 
-    const obj = {}
-
     try {
-      if (typeof window === "object") {
-        ;(window.adsbygoogle = window.adsbygoogle || []).push(obj)
-      }
+      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
       setLoading(false)
     } catch (e) {
       console.log(e)
     }
-  }, [])
+  }, [pathname, searchParams])
 
   return (
     <>
       {loading ? (
         <Skeleton className="mb-4 h-72 rounded-xl" />
       ) : (
-        <ins
-          className="adsbygoogle"
-          style={{ display: "block" }}
-          data-ad-client={env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}
-          data-ad-slot={content}
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        />
+        <>{children}</>
       )}
     </>
   )
