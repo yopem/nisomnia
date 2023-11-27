23205038ba983987b8b308373a3914ec5ff91531
karyanayandi <halo@yandi.me>
2023-11-27 18:31:58 +0700

    revert(apps/web): ads component

--- apps/web/src/components/Ad/client/Adsense.tsx
+++ apps/web/src/components/Ad/client/Adsense.tsx
@@ -15,16 +15,17 @@ export const Adsense: React.FunctionComponent<AdsenseProps> = (props) => {
 
   const [loading, setLoading] = React.useState<boolean>(true)
 
-  const initAd = () => {
-    if (typeof window === "object") {
-      window.adsbygoogle = window.adsbygoogle || []
-      window.adsbygoogle.push({})
-    }
-  }
-
   React.useEffect(() => {
     setLoading(true)
-    initAd()
+
+    try {
+      if (typeof window === "object") {
+        window.adsbygoogle = window.adsbygoogle || []
+        window.adsbygoogle.push({})
+      }
+    } catch (e) {
+      console.log(e)
+    }
     setLoading(false)
   }, [])
 
@@ -33,16 +34,14 @@ export const Adsense: React.FunctionComponent<AdsenseProps> = (props) => {
       {loading ? (
         <Skeleton className="mb-4 h-72 rounded-xl" />
       ) : (
-        <>
-          <ins
-            className="adsbygoogle"
-            style={{ display: "block", width: "100%" }}
-            data-ad-client={env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}
-            data-ad-slot={content}
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-        </>
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block", width: "100%" }}
+          data-ad-client={env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}
+          data-ad-slot={content}
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
       )}
     </>
   )
