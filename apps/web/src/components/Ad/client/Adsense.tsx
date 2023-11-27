048008f9dbe08cc62eec557d49ca5b33d64fbdbb
karyanayandi <halo@yandi.me>
2023-11-27 13:45:56 +0700

    feat(apps/web): adsense

--- apps/web/src/components/Ad/client/Adsense.tsx
+++ apps/web/src/components/Ad/client/Adsense.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import * as React from "react"
-import { usePathname, useSearchParams } from "next/navigation"
-import Script from "next/script"
 
 import { Skeleton } from "@nisomnia/ui"
 
@@ -17,29 +15,15 @@ export const Adsense: React.FunctionComponent<AdsenseProps> = (props) => {
 
   const [loading, setLoading] = React.useState<boolean>(true)
 
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+  const initAd = () => {
+    ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+  }
 
   React.useEffect(() => {
     setLoading(true)
-
-    const handleAdsenseLoad = () => {
-      setLoading(false)
-    }
-
-    try {
-      // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
-      if (window.adsbygoogle && window.adsbygoogle.push) {
-        window.adsbygoogle.push({})
-      } else {
-        window.adsbygoogle = {
-          push: handleAdsenseLoad,
-        }
-      }
-    } catch (e) {
-      console.log(e)
-    }
-  }, [pathname, searchParams])
+    initAd()
+    setLoading(false)
+  }, [])
 
   return (
     <>
@@ -47,15 +31,6 @@ export const Adsense: React.FunctionComponent<AdsenseProps> = (props) => {
         <Skeleton className="mb-4 h-72 rounded-xl" />
       ) : (
         <>
-          {process.env.APP_ENV === "production" && (
-            <Script
-              id="adsense"
-              async
-              src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
-              strategy="afterInteractive"
-              crossOrigin="anonymous"
-            />
-          )}
           <ins
             className="adsbygoogle"
             style={{ display: "block", width: "100%" }}
