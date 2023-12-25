f222dd46590da8d369cca4cf8a9ea098bf294cca
karyanayandi <halo@yandi.me>
2023-12-25 13:15:26 +0700

    fix(packages/api): handle error media

--- packages/api/src/modules/media/media.route.ts
+++ packages/api/src/modules/media/media.route.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server"
 import { z } from "zod"
 
 import {
@@ -12,42 +13,84 @@ export const mediaRouter = createTRPCRouter({
   all: adminProtectedProcedure
     .input(z.object({ page: z.number(), per_page: z.number() }))
     .query(async ({ ctx, input }) => {
-      return await ctx.db.media.findMany({
-        orderBy: {
-          createdAt: "desc",
-        },
-        skip: (input.page - 1) * input.per_page,
-        take: input.per_page,
-        select: {
-          id: true,
-          name: true,
-          description: true,
-          url: true,
-          author: {
-            select: {
-              username: true,
-            },
+      try {
+        const data = await ctx.db.media.findMany({
+          orderBy: {
+            createdAt: "desc",
           },
-          createdAt: true,
-          updatedAt: true,
-        },
-      })
+          skip: (input.page - 1) * input.per_page,
+          take: input.per_page,
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            url: true,
+            author: {
+              select: {
+                username: true,
+              },
+            },
+            createdAt: true,
+            updatedAt: true,
+          },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Media not found",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   dashboard: adminProtectedProcedure
     .input(z.object({ page: z.number(), per_page: z.number() }))
     .query(async ({ ctx, input }) => {
-      return await ctx.db.media.findMany({
-        orderBy: {
-          createdAt: "desc",
-        },
-        skip: (input.page - 1) * input.per_page,
-        take: input.per_page,
-        select: {
-          id: true,
-          url: true,
-          name: true,
-        },
-      })
+      try {
+        const data = await ctx.db.media.findMany({
+          orderBy: {
+            createdAt: "desc",
+          },
+          skip: (input.page - 1) * input.per_page,
+          take: input.per_page,
+          select: {
+            id: true,
+            url: true,
+            name: true,
+          },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Media not found",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   dashboardInfinite: adminProtectedProcedure
     .input(
@@ -57,91 +100,152 @@ export const mediaRouter = createTRPCRouter({
       }),
     )
     .query(async ({ ctx, input }) => {
-      const limit = input.limit ?? 50
+      try {
+        const limit = input.limit ?? 50
 
-      const cursorCondition = input.cursor
-        ? {
-            updatedAt: {
-              lt: new Date(input.cursor),
-            },
+        const cursorCondition = input.cursor
+          ? {
+              updatedAt: {
+                lt: new Date(input.cursor),
+              },
+            }
+          : {}
+
+        const medias = await ctx.db.media.findMany({
+          where: {
+            AND: [cursorCondition],
+          },
+          take: limit + 1,
+          orderBy: {
+            updatedAt: "desc",
+          },
+          select: {
+            id: true,
+            name: true,
+            url: true,
+            type: true,
+            description: true,
+            author_id: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        })
+
+        let nextCursor: string | undefined = undefined
+
+        if (medias.length > limit) {
+          const nextItem = medias.pop()
+          if (nextItem?.updatedAt) {
+            nextCursor = nextItem.updatedAt.toISOString()
           }
-        : {}
-
-      const medias = await ctx.db.media.findMany({
-        where: {
-          AND: [cursorCondition],
-        },
-        take: limit + 1,
-        orderBy: {
-          updatedAt: "desc",
-        },
-        select: {
-          id: true,
-          name: true,
-          url: true,
-          type: true,
-          description: true,
-          author_id: true,
-          createdAt: true,
-          updatedAt: true,
-        },
-      })
-
-      let nextCursor: string | undefined = undefined
-
-      if (medias.length > limit) {
-        const nextItem = medias.pop()
-        if (nextItem?.updatedAt) {
-          nextCursor = nextItem.updatedAt.toISOString()
         }
-      }
 
-      return {
-        medias,
-        nextCursor,
+        if (!medias) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Media not found",
+          })
+        }
+
+        return {
+          medias,
+          nextCursor,
+        }
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
       }
     }),
   byId: adminProtectedProcedure
     .input(z.string())
     .query(async ({ ctx, input }) => {
-      return await ctx.db.media.findUnique({
-        where: { id: input },
-        select: {
-          id: true,
-          name: true,
-          description: true,
-          url: true,
-          author: {
-            select: {
-              name: true,
-              username: true,
+      try {
+        const data = await ctx.db.media.findUnique({
+          where: { id: input },
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            url: true,
+            author: {
+              select: {
+                name: true,
+                username: true,
+              },
             },
+            createdAt: true,
+            updatedAt: true,
           },
-          createdAt: true,
-          updatedAt: true,
-        },
-      })
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Media not found",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   byName: adminProtectedProcedure
     .input(z.string())
     .query(async ({ ctx, input }) => {
-      return await ctx.db.media.findUnique({
-        where: { name: input },
-        select: {
-          id: true,
-          name: true,
-          description: true,
-          url: true,
-          author: {
-            select: {
-              name: true,
-              username: true,
+      try {
+        const data = await ctx.db.media.findUnique({
+          where: { name: input },
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            url: true,
+            author: {
+              select: {
+                name: true,
+                username: true,
+              },
             },
+            createdAt: true,
+            updatedAt: true,
           },
-          createdAt: true,
-          updatedAt: true,
-        },
-      })
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Media not found",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   byAuthorId: adminProtectedProcedure
     .input(
@@ -152,72 +256,219 @@ export const mediaRouter = createTRPCRouter({
       }),
     )
     .query(async ({ ctx, input }) => {
-      return await ctx.db.media.findMany({
-        where: { author_id: input.author_id },
-        orderBy: {
-          createdAt: "desc",
-        },
-        skip: (input.page - 1) * input.per_page,
-        take: input.per_page,
-        select: {
-          id: true,
-          url: true,
-          name: true,
-        },
-      })
+      try {
+        const data = await ctx.db.media.findMany({
+          where: { author_id: input.author_id },
+          orderBy: {
+            createdAt: "desc",
+          },
+          skip: (input.page - 1) * input.per_page,
+          take: input.per_page,
+          select: {
+            id: true,
+            url: true,
+            name: true,
+          },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Media not found",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   search: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
-    return await ctx.db.media.findMany({
-      where: {
-        name: {
-          contains: input,
-          mode: "insensitive",
+    try {
+      const data = await ctx.db.media.findMany({
+        where: {
+          name: {
+            contains: input,
+            mode: "insensitive",
+          },
         },
-      },
-      select: {
-        id: true,
-        name: true,
-        url: true,
-      },
-    })
+        select: {
+          id: true,
+          name: true,
+          url: true,
+        },
+      })
+
+      if (!data) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Media not found",
+        })
+      }
+
+      return data
+    } catch (error) {
+      console.error("Error:", error)
+      if (error instanceof TRPCError) {
+        throw error
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An internal error occurred",
+        })
+      }
+    }
   }),
   count: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.db.media.count()
+    try {
+      const data = await ctx.db.media.count()
+
+      if (!data) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Media not found",
+        })
+      }
+
+      return data
+    } catch (error) {
+      console.error("Error:", error)
+      if (error instanceof TRPCError) {
+        throw error
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An internal error occurred",
+        })
+      }
+    }
   }),
   create: protectedProcedure
     .input(uploadMediaSchema)
     .mutation(async ({ ctx, input }) => {
-      return await ctx.db.media.create({
-        data: {
-          name: input.name,
-          type: input.type,
-          url: input.url,
-          description: input.description,
-          author_id: ctx.session.user.id,
-        },
-      })
+      try {
+        const data = await ctx.db.media.create({
+          data: {
+            name: input.name,
+            type: input.type,
+            url: input.url,
+            description: input.description,
+            author_id: ctx.session.user.id,
+          },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Failed to create media",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   update: adminProtectedProcedure
     .input(updateMediaSchema)
     .mutation(async ({ ctx, input }) => {
-      return await ctx.db.media.update({
-        where: {
-          id: input.id,
-        },
-        data: {
-          ...input,
-          updatedAt: new Date(),
-        },
-      })
+      try {
+        const data = await ctx.db.media.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            ...input,
+            updatedAt: new Date(),
+          },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Failed to update media",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   deleteById: adminProtectedProcedure
     .input(z.string())
     .mutation(async ({ ctx, input }) => {
-      return await ctx.db.media.delete({ where: { id: input } })
+      try {
+        const data = await ctx.db.media.delete({ where: { id: input } })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Failed to delete media",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   deleteByName: adminProtectedProcedure
     .input(z.string())
     .mutation(async ({ ctx, input }) => {
-      return await ctx.db.media.delete({ where: { name: input } })
+      try {
+        const data = await ctx.db.media.delete({ where: { name: input } })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Failed to delete media",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
 })
