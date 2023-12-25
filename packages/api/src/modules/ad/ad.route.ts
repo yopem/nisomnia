7eb3427aae1e6440f34e4e1cc2354f49326fd226
karyanayandi <halo@yandi.me>
2023-12-25 12:17:51 +0700

    fix(packages/api): handle ad error

--- packages/api/src/modules/ad/ad.route.ts
+++ packages/api/src/modules/ad/ad.route.ts
@@ -12,95 +12,229 @@ export const adRouter = createTRPCRouter({
   all: publicProcedure
     .input(z.object({ page: z.number(), per_page: z.number() }))
     .query(async ({ ctx, input }) => {
-      return await ctx.db.ad.findMany({
-        orderBy: {
-          createdAt: "desc",
-        },
-        skip: (input.page - 1) * input.per_page,
-        take: input.per_page,
-        select: {
-          id: true,
-          title: true,
-          content: true,
-          position: true,
-          type: true,
-          active: true,
-          createdAt: true,
-          updatedAt: true,
-        },
-      })
+      try {
+        const data = await ctx.db.ad.findMany({
+          orderBy: {
+            createdAt: "desc",
+          },
+          skip: (input.page - 1) * input.per_page,
+          take: input.per_page,
+          select: {
+            id: true,
+            title: true,
+            content: true,
+            position: true,
+            type: true,
+            active: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Ads not found",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   byId: adminProtectedProcedure
     .input(z.string())
     .query(async ({ ctx, input }) => {
-      return await ctx.db.ad.findUnique({
-        where: { id: input },
-        select: {
-          id: true,
-          title: true,
-          content: true,
-          position: true,
-          type: true,
-          active: true,
-          createdAt: true,
-          updatedAt: true,
-        },
-      })
+      try {
+        const data = await ctx.db.ad.findUnique({
+          where: { id: input },
+          select: {
+            id: true,
+            title: true,
+            content: true,
+            position: true,
+            type: true,
+            active: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Ad not found",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
   byPosition: publicProcedure
     .input(z.enum(AD_POSITION))
     .query(async ({ ctx, input }) => {
-      return await ctx.db.ad.findMany({
-        where: { position: input },
-        select: {
-          id: true,
-          title: true,
-          content: true,
-          position: true,
-          type: true,
-          active: true,
-          createdAt: true,
-          updatedAt: true,
-        },
-      })
-    }),
-  count: adminProtectedProcedure.query(async ({ ctx }) => {
-    return await ctx.db.ad.count()
-  }),
-  create: adminProtectedProcedure
-    .input(createAdSchema)
-    .mutation(async ({ ctx, input }) => {
-      return await ctx.db.ad.create({
-        data: input,
-      })
-    }),
-  update: adminProtectedProcedure
-    .input(updateAdSchema)
-    .mutation(async ({ ctx, input }) => {
-      return await ctx.db.ad.update({
-        where: {
-          id: input.id,
-        },
-        data: {
-          ...input,
-          updatedAt: new Date(),
-        },
-      })
-    }),
-  delete: adminProtectedProcedure
-    .input(z.string())
-    .mutation(async ({ ctx, input }) => {
-      const ad = await ctx.db.ad.findUnique({
-        where: { id: input },
-      })
+      try {
+        const data = await ctx.db.ad.findMany({
+          where: { position: input },
+          select: {
+            id: true,
+            title: true,
+            content: true,
+            position: true,
+            type: true,
+            active: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        })
 
-      if (!ad) {
+        if (!data) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Ad not found",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
         throw new TRPCError({
           code: "NOT_FOUND",
           message: "Ad not found",
         })
       }
+    }),
+  count: adminProtectedProcedure.query(async ({ ctx }) => {
+    try {
+      const data = await ctx.db.ad.count()
 
-      return await ctx.db.ad.delete({ where: { id: input } })
+      if (!data) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Ads not found",
+        })
+      }
+
+      return data
+    } catch (error) {
+      console.error("Error:", error)
+      if (error instanceof TRPCError) {
+        throw error
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An internal error occurred",
+        })
+      }
+    }
+  }),
+  create: adminProtectedProcedure
+    .input(createAdSchema)
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const data = await ctx.db.ad.create({
+          data: input,
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Ad not created",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
+    }),
+  update: adminProtectedProcedure
+    .input(updateAdSchema)
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const data = await ctx.db.ad.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            ...input,
+            updatedAt: new Date(),
+          },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Ad not updated",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
+    }),
+  delete: adminProtectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const data = await ctx.db.ad.delete({
+          where: { id: input },
+        })
+
+        if (!data) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Failed to delete ad",
+          })
+        }
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
     }),
 })
