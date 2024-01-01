ba4ca257bd12decdbf1e598a6d286dc5a7893096
karyanayandi <halo@yandi.me>
2024-01-01 07:45:04 +0700

    feat(apps/web): enable google analytics by env

--- apps/web/src/components/Scripts.tsx
+++ apps/web/src/components/Scripts.tsx
@@ -8,17 +8,21 @@ export const Scripts = () => {
     return (
       <>
         <AdsenseScript />
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-        />
-        <Script id="google-analytics">
-          {`
+        {process.env.ENFORCE_GA && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            />
+            <Script id="google-analytics">
+              {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
-        </Script>
+            </Script>
+          </>
+        )}
       </>
     )
   }
