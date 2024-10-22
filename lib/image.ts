f5362ab87c8f46c2c8ae5b4338bd9b2706a3cd92
ghizzkira <tiadiagis@gmail.com>
2024-10-22 19:42:22 +0700

    fix change image resizer

--- lib/image.ts
+++ lib/image.ts
@@ -6,11 +6,10 @@ export const resizeImage = async (buffer: Buffer) => {
 
   let newWidth = metadata.width ?? 1280
 
-  // Tentukan ukuran resize berdasarkan kondisi lebar gambar
   if (metadata.width && metadata.width > 1500) {
-    newWidth = 1280 // Set ukuran jadi 1280px jika lebar lebih dari 1500px
+    newWidth = 1280
   } else if (metadata.width && metadata.width <= 1500) {
-    newWidth = Math.floor(metadata.width * 0.7) // Turunkan 70% jika lebar <= 1500px
+    newWidth = Math.floor(metadata.width * 0.7)
   }
 
   // Melakukan resize gambar
