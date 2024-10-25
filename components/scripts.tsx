3a3f2d379bb3475a8ca6898b127ef5ab484ee7ae
karyanayandi <xkaryanayandi@gmail.com>
2024-10-26 05:37:53 +0700

    try using partytown with adsense

--- components/scripts.tsx
+++ components/scripts.tsx
@@ -1,16 +1,22 @@
 import Script from "next/script"
 
 import env from "@/env"
-import AdsenseScript from "./ad/adsense-script"
+
+// import AdsenseScript from "./ad/adsense-script"
 
 const Scripts = () => {
   if (process.env.APP_ENV === "production") {
     return (
       <>
-        <AdsenseScript />
+        {/* <AdsenseScript /> */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GA_ID}`}
           strategy="worker"
+        />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4946821479056257"
+          crossOrigin="anonymous"
         />
         <script type="text/partytown">
           {`
