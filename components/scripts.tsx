02de249b825788b9ec05af0f46869c11e02fed91
karyanayandi <xkaryanayandi@gmail.com>
2024-10-26 07:16:18 +0700

    try vanilla partytown

--- components/scripts.tsx
+++ components/scripts.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script"
 import { GoogleAnalytics } from "@next/third-parties/google"
 
 import env from "@/env"
@@ -10,11 +9,12 @@ const Scripts = () => {
       <>
         <AdsenseScript />
         <GoogleAnalytics gaId={env.NEXT_PUBLIC_GA_ID} />
-        <Script
+        <script
           defer
           src="https://data.dafunda.com/script.js"
           data-website-id="433dc72b-94a6-4fcd-92f2-3403a60f47c7"
-        />
+          type="text/partytown"
+        ></script>
       </>
     )
   }
