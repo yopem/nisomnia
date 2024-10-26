1add6a21b24b2e1f94845caa132179762e682c12
karyanayandi <xkaryanayandi@gmail.com>
2024-10-26 08:11:43 +0700

    update analytics

--- components/scripts.tsx
+++ components/scripts.tsx
@@ -1,4 +1,5 @@
 import { Partytown } from "@builder.io/partytown/react"
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 import env from "@/env"
 import AdsenseScript from "./ad/adsense-script"
@@ -15,21 +16,7 @@ const Scripts = () => {
           data-website-id="fce06352-f464-4337-a716-b78f9f0b46c3"
           type="text/partytown"
         ></script>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GA_ID}`}
-          type="text/partytown"
-        ></script>
-        <script
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                window.gtag = function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${env.NEXT_PUBLIC_GA_ID}')`,
-          }}
-        />
+        <GoogleAnalytics gaId={env.NEXT_PUBLIC_GA_ID} />
       </>
     )
   }
