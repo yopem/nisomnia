2e683ad1862cb7a44ad9277924cb7e85006be098
karyanayandi <xkaryanayandi@gmail.com>
2024-10-27 22:12:14 +0700

    add get media by category route

--- lib/api/routes/media.ts
+++ lib/api/routes/media.ts
@@ -228,6 +228,27 @@ export const mediaRouter = createTRPCRouter({
       }
     }
   }),
+  byCategory: publicProcedure
+    .input(mediaCategory)
+    .query(async ({ ctx, input }) => {
+      try {
+        const data = await ctx.db.query.medias.findMany({
+          where: (medias, { eq }) => eq(medias.category, input),
+        })
+
+        return data
+      } catch (error) {
+        console.error("Error:", error)
+        if (error instanceof TRPCError) {
+          throw error
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "An internal error occurred",
+          })
+        }
+      }
+    }),
   searchByCategory: publicProcedure
     .input(
       z.object({
