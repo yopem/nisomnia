0906a0ef9293c1d5b186d164cd0376f30566b475
karyanayandi <xkaryanayandi@gmail.com>
2024-10-28 11:27:49 +0700

    update

--- components/ad/adsense.tsx
+++ components/ad/adsense.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import * as React from "react"
-import { usePathname } from "next/navigation"
 import { AdUnit } from "next-google-adsense"
 
 import { Skeleton } from "@/components/ui/skeleton"
@@ -14,18 +13,13 @@ interface AdsenseProps extends React.HTMLAttributes<HTMLDivElement> {
 const Adsense: React.FC<AdsenseProps> = (props) => {
   const { content } = props
 
-  const pathname = usePathname()
-
   return (
     <React.Suspense
       fallback={
         <Skeleton className="[200px] my-[5px] flex min-h-[250px] w-screen min-w-full justify-center overflow-hidden sm:w-full" />
       }
     >
-      <div
-        className="my-[5px] flex h-auto w-screen min-w-full justify-center overflow-hidden sm:w-full"
-        key={pathname.replace(/\//g, "-") + "-" + content}
-      >
+      <div className="my-[5px] flex h-auto w-screen min-w-full justify-center overflow-hidden sm:w-full">
         <AdUnit
           publisherId={env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}
           slotId={content}
