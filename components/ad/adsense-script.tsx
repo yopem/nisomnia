29f1c71ab37c1ab82403f45c36369a36a6bcc74c
karyanayandi <xkaryanayandi@gmail.com>
2024-10-28 10:34:46 +0700

    add crossOrigin anonymous

--- components/ad/adsense-script.tsx
+++ components/ad/adsense-script.tsx
@@ -72,6 +72,7 @@ const AdsenseScript = () => {
     <Script
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
       strategy="afterInteractive"
+      crossOrigin="anonymous"
       onLoad={handleAdLoad}
     />
   )
