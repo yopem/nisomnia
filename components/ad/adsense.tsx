9bdef4e889902a628a0839b1d2f5f89c9a297566
karyanayandi <xkaryanayandi@gmail.com>
2024-10-28 11:01:21 +0700

    remove timeout

--- components/ad/adsense.tsx
+++ components/ad/adsense.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import * as React from "react"
-import { usePathname, useSearchParams } from "next/navigation"
+import { usePathname } from "next/navigation"
+import Script from "next/script"
 
 import { Skeleton } from "@/components/ui/skeleton"
 import env from "@/env"
@@ -14,65 +15,6 @@ const Adsense: React.FC<AdsenseProps> = (props) => {
   const { content } = props
 
   const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const [hasScrolled, setHasScrolled] = React.useState<boolean>(false)
-
-  const handleAdLoad = () => {
-    try {
-      const insElements = Array.from(
-        document.querySelectorAll("ins.manual-adsense"),
-      )
-      const insWithoutIframe = insElements.filter(
-        (ins) => !ins.querySelector("iframe"),
-      )
-      if (!hasScrolled && insWithoutIframe.length > 0) {
-        if (window.adsbygoogle) {
-          setHasScrolled(true)
-          insWithoutIframe.forEach((el) => {
-            if (!el.querySelector("iframe")) {
-              ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-            }
-          })
-        }
-      }
-    } catch (err) {
-      console.log("Err", err)
-    }
-  }
-
-  const handleAdScroll = () => {
-    const insElements = Array.from(
-      document.querySelectorAll("ins.manual-adsense"),
-    )
-    const insWithoutIframe = insElements.filter(
-      (ins) => !ins.querySelector("iframe"),
-    )
-    if (!hasScrolled && insWithoutIframe.length > 0) {
-      if (window.adsbygoogle) {
-        setHasScrolled(true)
-        insWithoutIframe.forEach((el) => {
-          if (!el.querySelector("iframe")) {
-            ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-          }
-        })
-      }
-    }
-  }
-
-  React.useEffect(() => {
-    const timeoutId = setTimeout(handleAdLoad, 9000)
-    window.addEventListener("scroll", handleAdScroll)
-
-    return () => {
-      clearTimeout(timeoutId)
-      window.removeEventListener("scroll", handleAdScroll)
-    }
-    //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hasScrolled, pathname, searchParams])
-
-  React.useEffect(() => {
-    setHasScrolled(false)
-  }, [pathname, searchParams])
 
   return (
     <React.Suspense
@@ -80,7 +22,10 @@ const Adsense: React.FC<AdsenseProps> = (props) => {
         <Skeleton className="[200px] my-[5px] flex min-h-[250px] w-screen min-w-full justify-center overflow-hidden sm:w-full" />
       }
     >
-      <div className="my-[5px] flex h-auto w-screen min-w-full justify-center overflow-hidden sm:w-full">
+      <div
+        className="my-[5px] flex h-auto w-screen min-w-full justify-center overflow-hidden sm:w-full"
+        key={pathname.replace(/\//g, "-") + "-" + content}
+      >
         <ins
           // eslint-disable-next-line tailwindcss/no-custom-classname
           className="adsbygoogle manual-adsense h-auto w-screen min-w-full sm:w-full"
@@ -90,6 +35,9 @@ const Adsense: React.FC<AdsenseProps> = (props) => {
           data-ad-format="auto"
           data-full-width-responsive="true"
         ></ins>
+        <Script id="next-google-adsense" strategy="afterInteractive">
+          (window.adsbygoogle = window.adsbygoogle || []).push({});
+        </Script>
       </div>
     </React.Suspense>
   )
