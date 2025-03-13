b50e7e89aa29f81e33c437c04cc9c3e0d13fd6e1
Karyana Yandi <karyana@yandi.me>
2025-03-14 02:41:21 +0700

    feat(db): add production company service functions
    Add database service functions for production companies including:
    - Get production company by slug
    - Get production companies sitemap data
    - Get count of published production companies

---
+++ src/server/db/service/production-company.ts
@@ -0,0 +1,41 @@
+import { count, eq } from "drizzle-orm"
+
+import { db } from "@/server/db"
+import { productionCompaniesTable } from "@/server/db/schema"
+
+export const getProductionCompnayBySlug = async (slug: string) => {
+  return await db.query.productionCompaniesTable.findFirst({
+    where: (productionCompany, { eq }) => eq(productionCompany.slug, slug),
+  })
+}
+
+export const getProductionCompaniesSitemap = async ({
+  page,
+  perPage,
+}: {
+  page: number
+  perPage: number
+}) => {
+  return await db.query.productionCompaniesTable.findMany({
+    where: (productionCompanies, { eq }) =>
+      eq(productionCompanies.status, "published"),
+    limit: perPage,
+    offset: (page - 1) * perPage,
+    orderBy: (productionCompanies, { desc }) => [
+      desc(productionCompanies.updatedAt),
+    ],
+    columns: {
+      slug: true,
+      updatedAt: true,
+    },
+  })
+}
+
+export const getProductionCompaniesCount = async () => {
+  const data = await db
+    .select({ value: count() })
+    .from(productionCompaniesTable)
+    .where(eq(productionCompaniesTable.status, "published"))
+
+  return data[0].value
+}
